4bc9b443131c63fe24d582113d771c4a835df9bb
RollieGarcia0031 <garciarollie827@gmail.com>
2026-03-04 11:54:30 +0800

    add activity 3

---
+++ act-3/src/Main.java
@@ -0,0 +1,31 @@
+package src;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        File stuff1Ref = new File("files/stuff1.txt");
+        File stuff2Ref = new File("files/stuff2.txt");
+
+        if (!stuff1Ref.exists()) {
+            System.out.println("stuff.txt file must exist!");
+            System.exit(0);
+        }
+
+        PrintWriter stuff2Writter = new PrintWriter(stuff2Ref);
+        Scanner stuff1 = new Scanner(stuff1Ref);
+
+
+        while (stuff1.hasNext()){
+            String line = stuff1.nextLine();
+            stuff2Writter.println( line );
+        }
+
+        stuff2Writter.close();
+
+    }
+
+}
